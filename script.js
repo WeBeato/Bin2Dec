f62d20cb51c36482cfac3693d50994c1c76a9924
webeato <webeato.ad@gmail.com>
2025-11-05 15:34:02 +0330

    update script and README

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-document.getElementById('convertBtn').addEventListener('click', () => {
-
-    let binaryInput = document.getElementById('binaryInput').value;
-    let result = document.getElementById('result');
+document.getElementById("convertBtn").addEventListener("click", () => {
+    let binaryInput = document.getElementById("binaryInput").value;
+    let result = document.getElementById("result");
+    let binaryRegex = /^[01]+$/;
 
     if (binaryInput.length > 32) {
-        result.style.color = 'red';
-        result.textContent = 'Input too long. Maximum 32 bits allowed.';
+        result.style.color = "red";
+        result.textContent = "Input too long. Maximum 32 bits allowed.";
     } else if (binaryInput.length < 4) {
-        result.style.color = 'red';
-        result.textContent = 'Input too short. Minimum 4 bits allowed.';
+        result.style.color = "red";
+        result.textContent = "Input too short. Minimum 4 bits allowed.";
     } else {
-
-
-        let decimal = parseInt(binaryInput, 2);
-        result.style.color = '#00ffcc';
-        result.textContent = decimal;
-
+        if (binaryRegex.test(binaryInput)) {
+            let decimal = parseInt(binaryInput, 2);
+            result.style.color = "#00ffcc";
+            result.textContent = decimal;
+        } else {
+            result.style.color = "red";
+            result.textContent = "Input is not valid.";
+        }
     }
-})
+});
